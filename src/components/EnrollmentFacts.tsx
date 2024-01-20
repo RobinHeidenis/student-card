c81b446d1edc0d636cdb181c6573ad0b1c30b126
Robin Heidenis <robin.heidenis@gmail.com>
2024-01-20 18:38:45 +0000

    feat: 🎸 make school year dynamic, to always use current year

--- src/components/EnrollmentFacts.tsx
+++ src/components/EnrollmentFacts.tsx
@@ -6,24 +6,30 @@ import {
 	RiSystemDashboardFill,
 } from "solid-icons/ri";
 
-export const EnrollmentFacts: Component = () => (
-	<div
-		class={
-			"w-11/12 bg-white drop-shadow-sm border border-gray-200 self-center rounded-lg pt-4 flex mb-20 flex-col gap-y-8 pr-5 pb-5"
-		}
-	>
-		<FactRow icon={FaRegularCalendar} title={"Academic year"} value={"2023"} />
-		<FactRow
-			icon={FaSolidClock}
-			title={"Valid"}
-			value={"From 1 September 2023 up to and including 31 August 2023"}
-		/>
-		<FactRow
-			icon={RiOthersGraduationCapFill}
-			title={"Course"}
-			value={"HBO-ICT"}
-		/>
-		<FactRow icon={FaSolidCode} title={"Code"} value={"ICT"} />
-		<FactRow icon={RiSystemDashboardFill} title={"Variant"} value={"Voltijd"} />
-	</div>
-);
+export const EnrollmentFacts: Component = () => {
+	const isAfterAugust = new Date().getMonth() >= 8;
+	const currentYear = new Date().getFullYear();
+	const academicYear = isAfterAugust ? currentYear : currentYear - 1;
+
+	return (
+		<div
+			class={
+				"w-11/12 bg-white drop-shadow-sm border border-gray-200 self-center rounded-lg pt-4 flex mb-20 flex-col gap-y-8 pr-5 pb-5"
+			}
+		>
+			<FactRow icon={FaRegularCalendar} title={"Academic year"} value={academicYear.toString()}/>
+			<FactRow
+				icon={FaSolidClock}
+				title={"Valid"}
+				value={`From 1 September ${academicYear} up to and including 31 August ${academicYear + 1}`}
+			/>
+			<FactRow
+				icon={RiOthersGraduationCapFill}
+				title={"Course"}
+				value={"HBO-ICT"}
+			/>
+			<FactRow icon={FaSolidCode} title={"Code"} value={"ICT"}/>
+			<FactRow icon={RiSystemDashboardFill} title={"Variant"} value={"Voltijd"}/>
+		</div>
+	);
+};
